f65d3ae9e673e529f390b057057e55d3d81f356c
Tsyrema Mansheeva <tsyremamansheeva@Tsyremas-Air.home>
2018-11-12 11:57:20 -0500

    Login and register separated

--- Locket/Locket/LandingViewController.swift
+++ Locket/Locket/LandingViewController.swift
@@ -11,22 +11,28 @@ import FirebaseDatabase
 
 class LandingViewController: UIViewController, UIAlertViewDelegate, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var RegisterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     
-    @IBAction func loginButtonTapped(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let livc = storyboard.instantiateViewController(withIdentifier: "LogInVC")
-        self.present(livc, animated: true, completion: nil)
-        
-    }
     
-    @IBAction func registerButtonTapped(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let rvc = storyboard.instantiateViewController(withIdentifier: "RegisterVC")
-        self.present(rvc, animated: true, completion: nil)
-    }
+//
+//    @IBAction func loginButtonTapped(_ sender: UIButton) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let livc = storyboard.instantiateViewController(withIdentifier: "LogInVC")
+//        self.present(livc, animated: true, completion: nil)
+//
+//    }
+//
+//    @IBAction func registerButtonTapped(_ sender: UIButton) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let rvc = storyboard.instantiateViewController(withIdentifier: "RegisterVC")
+//        self.present(rvc, animated: true, completion: nil)
+//    }
     
 }
